8602a504b40a8f10ba1dbb04efdcab257031b46b
shreeviknesh <luckyboy1998@gmail.com>
2020-05-28 20:28:57 +0530

    Added init.vim to nvim/

--- collect_dotfiles.sh
+++ collect_dotfiles.sh
@@ -1,11 +1,11 @@
 find . -maxdepth 1 -type f ! -name collect_dotfiles.sh ! -name distribute_dotfiles.sh -delete
 rm -rf nvim
+mkdir nvim/config -p
 
 cp ~/.bash_aliases ./.bash_aliases
 cp ~/.bashrc ./.bashrc
 cp ~/.p10k.zsh ./.p10k.zsh
 cp ~/.zshrc ./.zshrc
-cp ~/.config/nvim/init.vim ./init.vim
 
-mkdir nvim/config -p
+cp ~/.config/nvim/init.vim     ./nvim/init.vim
 cp ~/.config/nvim/config/*.vim ./nvim/config/
--- distribute_dotfiles.sh
+++ distribute_dotfiles.sh
@@ -1,8 +1,9 @@
+mkdir ~/.config/nvim/config -p
+
 cp .bash_aliases ~/.bash_aliases
 cp .bashrc ~/.bashrc
 cp .p10k.zsh ~/.p10k.zsh
 cp .zshrc ~/.zshrc
-cp init.vim ~/.config/nvim/init.vim
 
-mkdir ~/.config/nvim/config -p
+cp ./nvim/init.vim     ~/.config/nvim/init.vim
 cp ./nvim/config/*.vim ~/.config/nvim/config/
